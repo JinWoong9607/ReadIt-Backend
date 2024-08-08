0c7184f06022b4016242cf760dbcf0cdadcce6e5
elphabaa <slsl03264@naver.com>
2024-08-08 11:46:16 +0900

    no message

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'production';
-const path = require('path');
 const config = require('../config/config.js')[env];
 const db = {};
 
@@ -15,21 +17,23 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 });
 
-const User = require('./user');
-const Comment = require('./comment');
-const Dictionary = require('./dictionary');
+// 동적으로 모델 파일 로드
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
-db.User = User;
-db.Comment = Comment;
-db.Dictionary = Dictionary;
-
-User.init(sequelize);
-Comment.init(sequelize);
-Dictionary.init(sequelize);
-
-User.associate(db);
-Comment.associate(db);
-Dictionary.associate(db);
+// 모델 간 관계 설정
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
